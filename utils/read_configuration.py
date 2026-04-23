fa85e4e78fa0c05096dc84f56fe74789236f4777
pdo-sant <pedro.santosvale1999@gmail.com>
2026-04-23 17:35:43 -0300

    chore(comment): add coment in the code

--- utils/read_configuration.py
+++ utils/read_configuration.py
@@ -1,21 +1,37 @@
 import sys
 from .is_valid_keys import is_valid_keys
 
+"""
+Function for the read file that receive in sys.args 
+
+The function return a Dict[str, str].
+If the mmandatory fields no has in the file the program is stop
+"""
+
 
 def read_configuration(path: str) -> dict[str, str]:
+    # variable configuration of type Dict[str, str]
     configuration: dict[str, str] = {}
+    # 'Try' block is used to run code if it the fails her run 'Exept'
     try:
+        # Open the file. The file read line for line
         with open(path, "r") as file:
             for line in file:
+                # remove the white spaces in start and end file
                 line = line.strip()
+                # if the line for white or start the #
                 if (not line or line.startswith("#")):
                     continue
+                # if the line not have = in your caractes break the program
                 if ("=" not in line):
                     print("Erro: Sintaxe inválida")
                     sys.exit(1)
+                # create the variavles key and value and get the field in order
                 key, value = line.split("=", 1)
                 key, value = key.strip(), value.strip()
+                # add the value in the prop {'width':"50"}
                 configuration[key] = value
+        # verify if the file contains all the condiguration mandatory
         if (not is_valid_keys(configuration)):
             print("Not has all atributes mandatorys")
             sys.exit()
